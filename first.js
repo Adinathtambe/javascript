0f5befd278ebf7ea92c23609e168fa9aeaea3483
Adinathtambe <adinathtambe4@gmail.com>
2024-06-26 11:24:49 +0530

    javascript

---
+++ first.js
@@ -0,0 +1,17 @@
+// console.log("Hello RUshi");
+
+// let, const,var
+
+/*let fullName = "Adinath" ;
+var age ="25"
+console.log(fullName);
+console.log("age is:"+age);
+*/
+
+let age = 12;
+if (age >= 18){
+    console.log("you can vote.");
+}
+if (age < 18){
+    console.log("you can't vote.");
+}
